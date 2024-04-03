2e64c67e719d5e3a3e9bea1b5e96928f58a949c1
Jeroen Hulshof <jeroenhulshof@mbp-van-jeroen.kpn>
2024-04-03 21:01:18 +0200

    typo

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ DOTFILES="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Update dotfiles
 [ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
 
-# Install brew services
+# Run install scripts
 . "$DOTFILES/brew/brew.sh"
 . "$DOTFILES/brew/brew-cask.sh"
 . "$DOTFILES/composer/composer.sh"
